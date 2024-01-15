6982164c1e22a3eb9e18b1bf3dea5f4e416b7f2b
Nick <62834545+NickBodliev@users.noreply.github.com>
2024-01-15 10:02:26 +0100

    Create decoder.py

---
+++ decoder.py
@@ -0,0 +1,20 @@
+data = {}
+
+def decode(message_file):
+	with open(message_file, 'r') as file:
+		for line in file.readlines():
+			# Split each line into words
+			words = line.split()
+
+			# Extract the first and second words (assuming the first is an integer and the second is a string)
+			number = int(words[0])
+			string_data = words[1]
+
+			# Populate the dictionary
+			data[number] = string_data
+
+	return data[158]
+
+file_path = 'coding_qual_input.txt'
+decoded_message = decode(file_path)
+print(decoded_message)
